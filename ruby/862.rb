1607a66d85bc7f14dd091dfc3ed40c36478ed9c9
Mason <mason.a.traylor@gmail.com>
2019-12-12 13:07:31 -1000

    862.rb

---
+++ ruby/862.rb
@@ -0,0 +1,17 @@
+# @param {Integer[]} a
+# @param {Integer} k
+# @return {Integer}
+def shortest_subarray(a, k)
+  b = [0]
+  a.each {|i| b << b[-1] + i}
+  
+  result = a.size + 1
+  monoq = []
+  b.each_with_index do |bi, i|
+      monoq.pop until monoq.empty? || bi > b[monoq[-1]]
+      result = [result, i - monoq.shift].min until monoq.empty? || bi - b[monoq[0]] < k
+      monoq << i
+  end
+  
+  result < a.size + 1 ? result : -1
+end
