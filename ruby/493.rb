1370df5734a0bb648308626860394b84bdd1cebd
Mason <mason.a.traylor@gmail.com>
2019-12-12 13:12:11 -1000

    Add 493.rb

---
+++ ruby/493.rb
@@ -0,0 +1,16 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def reverse_pairs(nums)
+  pairs = 0
+  sorted = []
+  
+  nums.reverse_each do |num|
+      idx = sorted.bsearch_index { |i| i * 2 < num } || sorted.size
+      pairs += sorted.size - idx
+      
+      idx = sorted.bsearch_index { |i| i <= num } || sorted.size
+      sorted.insert(idx, num)
+  end
+  
+  pairs
+end
