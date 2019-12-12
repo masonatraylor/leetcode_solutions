bafe4735d5cfbb82336d5f672c5705a8d65c5edd
Mason <mason.a.traylor@gmail.com>
2019-12-12 13:18:45 -1000

    Add 132.rb

---
+++ ruby/132.rb
@@ -0,0 +1,19 @@
+# @param {String} s
+# @return {Integer}
+def min_cut(s)
+  cuts = (0...s.size).to_a
+  pal = Array.new(s.size) {Array.new(s.size, false)}
+  
+  s.size.times do |i|
+    si = s[i]
+    (0..i).each do |j|
+      if si == s[j] && (j + 1 >= i - 1 || pal[j+1][i-1])
+        pal[j][i] = true
+        count = j == 0 ? 0 : cuts[j-1] + 1
+        cuts[i] = count if count < cuts[i]
+      end
+    end
+  end
+  
+  cuts.last
+end
