8293a6fb11d34cda9c5e3ebdc8b0a413caf0e43e
Mason <mason.a.traylor@gmail.com>
2019-12-12 11:44:03 -1000

    Change indentation to two spaces

--- ruby/72.rb
+++ ruby/72.rb
@@ -5,15 +5,15 @@ def min_distance(word1, word2)
   dp = create_dp_array(word1.size + 1, word2.size + 1)
   
   word1.chars.each_with_index do |c1, i1|
-      word2.chars.each_with_index do |c2, i2|
-          if c1 == c2
-              dp[i1 + 1][i2 + 1] = dp[i1][i2]
-          else
-              dp[i1 + 1][i2 + 1] = 1 + [dp[i1 + 1][i2],
-                                        dp[i1][i2 + 1],
-                                        dp[i1][i2]].min
-          end
+    word2.chars.each_with_index do |c2, i2|
+      if c1 == c2
+        dp[i1 + 1][i2 + 1] = dp[i1][i2]
+      else
+        dp[i1 + 1][i2 + 1] = 1 + [dp[i1 + 1][i2],
+                                  dp[i1][i2 + 1],
+                                  dp[i1][i2]].min
       end
+    end
   end
   
   dp[-1][-1]
