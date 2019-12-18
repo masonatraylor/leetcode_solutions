f153f8c543b67563f1a7a476aa18c8bd07a45fb9
Mason <mason.a.traylor@gmail.com>
2019-12-17 19:19:06 -1000

    Refactor 23.rb

--- ruby/23.rb
+++ ruby/23.rb
@@ -18,15 +18,15 @@ def merge_k_lists(lists)
   while min = lists.shift
     previous.next = min if previous
     previous = min
-    insert_next(lists, min)
+    insert_next(lists, min.next)
   end
   
   head
 end
 
 private def insert_next(lists, node)
-  return if node.next.nil?
+  return if node.nil?
   
-  index = lists.bsearch_index { |i| i.val > node.next.val } || -1
-  lists.insert(index, node.next)
+  index = lists.bsearch_index { |i| i.val > node.val } || -1
+  lists.insert(index, node)
 end
