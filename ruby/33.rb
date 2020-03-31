fca365c6dce612ebd711cfe059c4b6bc2c73c769
Mason <mason.a.traylor@gmail.com>
2020-03-30 18:51:16 -0700

    Update 33.rb

--- ruby/33.rb
+++ ruby/33.rb
@@ -3,10 +3,6 @@
 # @return {Integer}
 def search(nums, target)
   pivot = nums.bsearch_index { |i| i < nums.first } || 0
-  [(0...pivot), (pivot...nums.size)].each do |range|
-    idx = nums[range].bsearch_index { |i| i >= target } || 0
-    return idx + range.first if nums[idx + range.first] == target
-  end
-  
-  -1
+  idx = (pivot - nums.size...pivot).bsearch { |i| nums[i] >= target } || 0
+  nums[idx] == target ? idx % nums.size : -1
 end
