cc30a1a37a63e311a3434c425750b8bfbdf69740
Mason <mason.a.traylor@gmail.com>
2020-04-01 07:27:01 -0700

    Update priority_queue.rb

--- ruby/priority_queue.rb
+++ ruby/priority_queue.rb
@@ -2,43 +2,68 @@ class PriorityQueue
   def initialize(&comp)
     # Defaults to a min heap
     @comp = comp || -> (a, b) { a < b }
-    @arr = []
+    @arr = [nil]
   end
   
   def offer(item)
     index = @arr.size
     
-    while index > 0 && @comp.call(item, @arr[(index - 1) / 2])
-      @arr[index] = @arr[(index - 1) / 2]
-      index = (index - 1) / 2
+    while index > 1 && @comp.call(item, @arr[index / 2])
+      @arr[index] = @arr[index / 2]
+      index /= 2
     end
     
     @arr[index] = item
   end
   
   def empty?
-    @arr.empty?
+    @arr.size == 1
   end
   
   def poll
-    result = @arr.first
+    return nil if empty?
+    
+    result = @arr[1]
     last = @arr.pop
-    
-    return result if @arr.empty?
-    
-    idx = 0
-    @arr[0] = last
+    @arr[1] = last unless empty?
+    bubble_down(1)
+    result
+  end
+  
+  private def bubble_down(idx)
     loop do
-      next_idx = idx * 2 + 1
+      next_idx = idx * 2
       next_idx += 1 if next_idx + 1 < @arr.size && @comp.call(@arr[next_idx + 1], @arr[next_idx])
 
-      return result if next_idx >= @arr.size || @comp.call(@arr[idx], @arr[next_idx])
+      return if next_idx >= @arr.size || @comp.call(@arr[idx], @arr[next_idx])
       
-      tmp = @arr[idx]
-      @arr[idx] = @arr[next_idx]
-      @arr[next_idx] = tmp
+      swap(idx, next_idx)
       idx = next_idx
     end
+  end
+  
+  private def swap(idx1, idx2)
+    tmp = @arr[idx1]
+    @arr[idx1] = @arr[idx2]
+    @arr[idx2] = tmp
+  end
+  
+  def first(n)
+    result = []
+    pq = PriorityQueue.new { |a, b| @comp.call(@arr[a], @arr[b]) }
+    pq.offer(1) unless empty?
+    
+    n.times do
+      break if pq.empty?
+      
+      idx = pq.poll
+      result << @arr[idx]
+      
+      pq.offer(idx * 2) if idx * 2 < @arr.size
+      pq.offer(idx * 2 + 1) if idx * 2 + 1 < @arr.size
+    end
+    
+    result
   end
 end
 
